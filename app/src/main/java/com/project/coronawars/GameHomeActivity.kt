26a4d9a6f506e84d8e02869e6fa140684b046e74
Ashish Kenjale <ashishkenjale@gmail.com>
2020-06-14 19:19:50 -0700

    Updated Home

--- app/src/main/java/com/project/coronawars/GameHomeActivity.kt
+++ app/src/main/java/com/project/coronawars/GameHomeActivity.kt
@@ -2,13 +2,15 @@ package com.project.coronawars
 
 import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Point
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_game_home.*
 
 class GameHomeActivity : AppCompatActivity() {
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_home)
@@ -18,9 +20,18 @@ class GameHomeActivity : AppCompatActivity() {
             startActivity(navigationIntent)
         }
 
+        controlsButton.setOnClickListener(View.OnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.controls_title))
+                .setMessage(getString(R.string.controls_text)                )
+                .setNegativeButton(
+                    "OK"
+                ) { dialog, which -> }.show()
+        })
+
         aboutButton.setOnClickListener(View.OnClickListener {
             AlertDialog.Builder(this)
-                .setTitle(getString(R.string.about_corona_wars))
+                .setTitle(getString(R.string.about_corona_wars_title))
                 .setMessage(getString(R.string.about_game_text)                )
                 .setNegativeButton(
                     "OK"
